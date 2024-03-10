ef96ab2cebc46d7d9fd678a727ab39665065c048
Kingsley Victor <javaprodigy56@gmail.com>
2024-03-10 03:06:14 +0100

    made updates

--- main/app/layout.tsx
+++ main/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@/i18n";
 import NavigationBar from "@/ui/NavigationBar";
 import OptionsBar from "@/ui/OptionsBar";
 import AllContexts from "@/contexts/all";
--- main/configs/web3.config.ts
+++ main/configs/web3.config.ts
@@ -1,9 +1,16 @@
 import { createClient } from "viem";
 import { http, createConfig } from "wagmi";
 import { berachainTestnet, sepolia } from "wagmi/chains";
+import { walletConnect, injected, coinbaseWallet, safe } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [berachainTestnet, sepolia],
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: "Moniswap" }),
+    safe(),
+    walletConnect({ projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string })
+  ],
   client({ chain }) {
     return createClient({ chain, transport: http() });
   }
